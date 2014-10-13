fc5ead13d83e97f808939a26bf5198b67aa96716
alireza <ithoughtful@gmail.com>
2014-10-13 23:08:58 +0200

    News class implementation

--- lib/hackernews/news.rb
+++ lib/hackernews/news.rb
@@ -1,10 +1,29 @@
 module HackerNews
 
-class News
+  class News
 
-  	 include HTTParty
-  	 base_uri 'http://api.ihackernews.com'
+    include HTTParty
+    base_uri 'http://api.ihackernews.com'
+    attr_reader :news_item
 
-end
+    def initialize(news_item)
+      @news_item = news_item
+    end
+
+    def self.get_news
+      get_items.map { |item|  new(item)}
+    end
+
+
+
+private
+    def self.get_items
+      get('/page')["items"]
+    end
+
+    def method_missing(name, *args, &block)
+      news_item.has_key?(name.to_s) ? news_item[name.to_s] : super
+    end 
+  end
 
 end
--- spec/lib/hackernews/news_spec.rb
+++ spec/lib/hackernews/news_spec.rb
@@ -5,22 +5,33 @@ describe HackerNews::News do
  
   describe "default attributes" do
  
-    it {should respond_to(:items)}
     it "has the base url set to the hackernews API endpoint" do
       expect(HackerNews::News.base_uri).to eq('http://api.ihackernews.com')
     end
  
   end
 
-  describe "get news items" do
-
+  describe "get_news" do
+    let(:news) {HackerNews::News.get_news}
     before do
-      VCR.insert_cassette 'player', :record => :new_episodes
+      VCR.insert_cassette 'news', :record => :new_episodes
     end
    
     after do
       VCR.eject_cassette
     end
+
+    it "returns array of news objects" do
+      expect(news.length).to be > 0
+    end
+
+    it "responds to news_item" do
+       expect(news[0]).to respond_to(:news_item)
+    end
+
+   xit "reponds to original hash keys" do
+    expect(news[0]).to respond_to("title")
+   end
   end
 
  end
