9f8d7adcb8a5ddb45655f7dde7766d5d30950074
alireza <ithoughtful@gmail.com>
2014-10-13 22:11:38 +0200

    initial commit

--- spec/lib/hackernews/news_spec.rb
+++ spec/lib/hackernews/news_spec.rb
@@ -5,10 +5,22 @@ describe HackerNews::News do
  
   describe "default attributes" do
  
+    it {should respond_to(:items)}
     it "has the base url set to the hackernews API endpoint" do
       expect(HackerNews::News.base_uri).to eq('http://api.ihackernews.com')
     end
  
+  end
+
+  describe "get news items" do
+
+    before do
+      VCR.insert_cassette 'player', :record => :new_episodes
+    end
+   
+    after do
+      VCR.eject_cassette
+    end
   end
 
  end
